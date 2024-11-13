761215d961d36c0f0129fd07c020fad64af126cf
WillyBoys <ethan.wilhelmsen9969@gmail.com>
2024-11-13 13:18:56 -0700

    More logic added

---
+++ client/src/main/java/Repl.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Repl {
+
+    private final UserInteraction interaction;
+
+    public Repl(String serverUrl) {
+        interaction = new UserInteraction(serverUrl);
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            String line = scanner.nextLine();
+
+            try {
+                result = interaction.eval(line);
+                System.out.print(result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+    }
+}
--- client/src/main/java/UserInteraction.java
+++ client/src/main/java/UserInteraction.java
@@ -1,3 +1,8 @@
+import chess.ChessGame;
+import exception.ResponseException;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 // THIS IS THE MAIN INTERACTIONS BETWEEN THE PROGRAM AND CLIENT
 public class UserInteraction {
@@ -9,5 +14,55 @@ public class UserInteraction {
         this.serverUrl = serverUrl;
     }
 
-    
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "register" -> registerUser(params);
+                case "login" -> loginUser(params);
+                case "logout" -> "logout";
+                case "list" -> listGames();
+                case "create" -> createGame(params);
+                case "join" -> joinGame(params);
+                case "clear" -> clear();
+                default -> help();
+            };
+        } catch (ResponseException ex) {
+            return ex.getMessage();
+        }
+    }
+
+    public String registerUser(String... params) throws ResponseException {
+
+    }
+
+    public String loginUser(String... params) throws ResponseException {
+
+    }
+
+    public String logoutUser() throws ResponseException {
+
+    }
+
+    public String listGames() throws ResponseException {
+
+    }
+
+    public String createGame(String... params) throws ResponseException {
+
+    }
+
+    public String joinGame(String... params) throws ResponseException {
+
+    }
+
+    public String clear() throws ResponseException {
+
+    }
+
+    public String help() throws ResponseException {
+
+    }
 }
