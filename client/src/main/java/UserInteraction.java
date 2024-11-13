b2a2930d2490591aa497b56bbc4301c3167061e2
WillyBoys <ethan.wilhelmsen9969@gmail.com>
2024-11-13 15:18:29 -0700

    Dry run of just adding what I thought

--- client/src/main/java/UserInteraction.java
+++ client/src/main/java/UserInteraction.java
@@ -1,13 +1,19 @@
 import chess.ChessGame;
+import com.google.gson.Gson;
 import exception.ResponseException;
 
 import java.util.Arrays;
 import java.util.Collection;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 // THIS IS THE MAIN INTERACTIONS BETWEEN THE PROGRAM AND CLIENT
 public class UserInteraction {
     private final ServerFacade server;
     private final String serverUrl;
+    private boolean loggedIn = false;
+    private String clientName = null;
 
     public UserInteraction(String serverUrl) {
         server = new ServerFacade(serverUrl);
@@ -22,7 +28,7 @@ public class UserInteraction {
             return switch (cmd) {
                 case "register" -> registerUser(params);
                 case "login" -> loginUser(params);
-                case "logout" -> "logout";
+                case "logout" -> logoutUser();
                 case "list" -> listGames();
                 case "create" -> createGame(params);
                 case "join" -> joinGame(params);
@@ -35,34 +41,80 @@ public class UserInteraction {
     }
 
     public String registerUser(String... params) throws ResponseException {
-
+        if (params.length >= 3) {
+            clientName = params[0];
+            server.registerUser();
+            loggedIn = true;
+            return String.format("You have registered as %s.", clientName);
+        }
+        throw new ResponseException(400, "Expected: <Username> <Password> <Email");
     }
 
     public String loginUser(String... params) throws ResponseException {
-
+        if (params.length >= 2) {
+            clientName = params[0];
+            server.loginUser();
+            loggedIn = true;
+            return String.format("You signed in as %s.", clientName);
+        }
+        throw new ResponseException(400, "Expected: <Username> <Password>");
     }
 
     public String logoutUser() throws ResponseException {
-
+        assertTrue(loggedIn);
+        server.logoutUser();
+        loggedIn = false;
+        return String.format("Logged out %s. We hate to see you go.", clientName);
     }
 
     public String listGames() throws ResponseException {
+        assertTrue(loggedIn);
+        var games = server.listGames();
+        var result = new StringBuilder();
+        var gson = new Gson();
+        for (var game : games) {
+            result.append(gson.toJson(game)).append('\n');
+        }
+        return result.toString();
 
     }
 
     public String createGame(String... params) throws ResponseException {
-
+        assertTrue(loggedIn);
+        if (params.length >= 1) {
+            String gameName = params[0];
+            server.createGame(gameName);
+        }
+        throw new ResponseException(400, "Expected: <GameName>");
     }
 
     public String joinGame(String... params) throws ResponseException {
-
+        assertTrue(loggedIn);
+        if (params.length >= 2) {
+            server.joinGame();
+        }
+        throw new ResponseException(400, "Expected: <GameID> <WHITE or BLACK>");
     }
 
     public String clear() throws ResponseException {
-
+        assertTrue(loggedIn);
+        server.clear();
+        return String.format("Databases Cleared by %s", clientName);
     }
 
     public String help() throws ResponseException {
-
+        if (!loggedIn) {
+            //Put the first options here
+            return """
+                    - Register New User: register <Username> <Password> <email>
+                    - Log Into Previous Account: login <Username> <Password>
+                    """;
+        }
+        return """
+                - Logout: logout
+                - List all Games: list
+                - Create New Game: create <GameName>
+                - Join a Game: join <GameID> <WHITE or BLACK>
+                """;
     }
 }
