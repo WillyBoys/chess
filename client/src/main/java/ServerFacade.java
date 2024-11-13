f8749712f7e78984774867a073cf5beebbaeaf49
WillyBoys <ethan.wilhelmsen9969@gmail.com>
2024-11-12 21:01:23 -0700

    Added some logic that I think is needed

--- client/src/main/java/ServerFacade.java
+++ client/src/main/java/ServerFacade.java
@@ -1,8 +1,38 @@
-
+import exception.ResponseException;
 
 public class ServerFacade {
 
-    public void registerUser () throws {
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+    public void registerUser() throws ResponseException {
+        
+    }
+
+    public void loginUser() throws ResponseException {
+
+    }
+
+    public void logoutUser() throws ResponseException {
+
+    }
+
+    public void listGames() throws ResponseException {
+
+    }
+
+    public void createGame() throws ResponseException {
+
+    }
+
+    public void joinGame() throws ResponseException {
+
+    }
+
+    public void clear() throws ResponseException {
 
     }
 }
