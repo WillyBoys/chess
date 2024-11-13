779bb91e425fee5e4c58dcf390294986dd210ba0
WillyBoys <ethan.wilhelmsen9969@gmail.com>
2024-11-13 07:36:08 -0700

    Logic Added from PetShop

--- client/src/main/java/ServerFacade.java
+++ client/src/main/java/ServerFacade.java
@@ -1,4 +1,13 @@
+import com.google.gson.Gson;
 import exception.ResponseException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
 
 public class ServerFacade {
 
@@ -9,30 +18,83 @@ public class ServerFacade {
     }
 
     public void registerUser() throws ResponseException {
-        
+        var path = "/user";
+        this.makeRequest("POST", path);
     }
 
     public void loginUser() throws ResponseException {
-
+        var path = "/session";
+        this.makeRequest("POST", path);
     }
 
     public void logoutUser() throws ResponseException {
-
+        var path = "/session";
+        this.makeRequest("DELETE", path);
     }
 
     public void listGames() throws ResponseException {
-
+        var path = "/game";
+        this.makeRequest("GET", path);
     }
 
     public void createGame() throws ResponseException {
-
+        var path = "/game";
+        this.makeRequest("POST", path);
     }
 
     public void joinGame() throws ResponseException {
-
+        var path = "/game";
+        this.makeRequest("PUT", path);
     }
 
     public void clear() throws ResponseException {
+        var path = "/db";
+        this.makeRequest("DELETE", path);
+    }
 
+    private <T> T makeRequest(String method, String path) throws ResponseException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            throw new ResponseException(status, "failure: " + status);
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
     }
 }
