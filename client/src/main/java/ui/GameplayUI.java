2937f80e0e74fb7e7ffd8d2f057cae7f230f2fd3
WillyBoys <ethan.wilhelmsen9969@gmail.com>
2024-11-14 08:02:41 -0700

    Messed with UI

--- client/src/main/java/ui/GameplayUI.java
+++ client/src/main/java/ui/GameplayUI.java
@@ -10,12 +10,12 @@ public class GameplayUI {
 
     // Board dimensions.
     private static final int BOARD_SIZE_IN_SQUARES = 8;
-    private static final int SQUARE_SIZE_IN_PADDED_CHARS = 8;
+    private static final int SQUARE_SIZE_IN_PADDED_CHARS = 3;
     private static final int LINE_WIDTH_IN_PADDED_CHARS = 1;
 
     // Padded characters.
     private static final String EMPTY = "   ";
-    
+
     public static void main(String[] args) {
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
 
@@ -23,7 +23,7 @@ public class GameplayUI {
 
         drawHeaders(out);
 
-        drawTicTacToeBoard(out);
+        drawChessBoard(out);
 
         out.print(SET_BG_COLOR_BLACK);
         out.print(SET_TEXT_COLOR_WHITE);
@@ -33,7 +33,7 @@ public class GameplayUI {
 
         setBlack(out);
 
-        String[] headers = {"TIC", "TAC", "TOE"};
+        String[] headers = {"1", "2", "3", "4", "5", "6", "7", "8"};
         for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
             drawHeader(out, headers[boardCol]);
 
@@ -46,6 +46,7 @@ public class GameplayUI {
     }
 
     private static void drawHeader(PrintStream out, String headerText) {
+        //Centers the header in the middle of the board
         int prefixLength = SQUARE_SIZE_IN_PADDED_CHARS / 2;
         int suffixLength = SQUARE_SIZE_IN_PADDED_CHARS - prefixLength - 1;
 
@@ -55,6 +56,7 @@ public class GameplayUI {
     }
 
     private static void printHeaderText(PrintStream out, String player) {
+        //Edit the look of the header
         out.print(SET_BG_COLOR_BLACK);
         out.print(SET_TEXT_COLOR_GREEN);
 
@@ -63,7 +65,7 @@ public class GameplayUI {
         setBlack(out);
     }
 
-    private static void drawTicTacToeBoard(PrintStream out) {
+    private static void drawChessBoard(PrintStream out) {
 
         for (int boardRow = 0; boardRow < BOARD_SIZE_IN_SQUARES; ++boardRow) {
 
@@ -71,7 +73,6 @@ public class GameplayUI {
 
             if (boardRow < BOARD_SIZE_IN_SQUARES - 1) {
                 // Draw horizontal row separator.
-                drawHorizontalLine(out);
                 setBlack(out);
             }
         }
@@ -88,35 +89,15 @@ public class GameplayUI {
                     int suffixLength = SQUARE_SIZE_IN_PADDED_CHARS - prefixLength - 1;
 
                     out.print(EMPTY.repeat(prefixLength));
-                    printPlayer(out, rand.nextBoolean() ? X : O);
+                    printPlayer(out, "X");
                     out.print(EMPTY.repeat(suffixLength));
                 } else {
                     out.print(EMPTY.repeat(SQUARE_SIZE_IN_PADDED_CHARS));
                 }
 
-                if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
-                    // Draw vertical column separator.
-                    setRed(out);
-                    out.print(EMPTY.repeat(LINE_WIDTH_IN_PADDED_CHARS));
-                }
-
                 setBlack(out);
             }
 
-            out.println();
-        }
-    }
-
-    private static void drawHorizontalLine(PrintStream out) {
-
-        int boardSizeInSpaces = BOARD_SIZE_IN_SQUARES * SQUARE_SIZE_IN_PADDED_CHARS +
-                (BOARD_SIZE_IN_SQUARES - 1) * LINE_WIDTH_IN_PADDED_CHARS;
-
-        for (int lineRow = 0; lineRow < LINE_WIDTH_IN_PADDED_CHARS; ++lineRow) {
-            setRed(out);
-            out.print(EMPTY.repeat(boardSizeInSpaces));
-
-            setBlack(out);
             out.println();
         }
     }
